4ba355e36eaf5d88c2b37c638088b32cf49d5456
David-Tarlos <david.tarlos@edu.tbz.ch>
2025-09-16 08:55:18 +0200

    add required frontend and backend

---
+++ Backend/Filmora/src/main/java/com/absolute/filmora/FilmoraApplication.java
@@ -0,0 +1,12 @@
+package com.absolute.filmora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmoraApplication.class, args);
+    }
+}
---
+++ Backend/Filmora/src/test/java/com/absolute/filmora/FilmoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.absolute.filmora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
