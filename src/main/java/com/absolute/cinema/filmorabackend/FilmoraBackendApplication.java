eb7f4dfd6815f04099586858f219460837bdca81
David-Tarlos <david.tarlos@edu.tbz.ch>
2025-09-16 09:50:20 +0200

    add Backend

---
+++ src/main/java/com/absolute/cinema/filmorabackend/FilmoraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.absolute.cinema.filmorabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmoraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmoraBackendApplication.class, args);
+	}
+
+}
